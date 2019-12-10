3a0e6c76199050243a56d26c1adf0616406d4adb
Fábio Caldeira <fabioandregoiscaldeira@outlook.com>
2019-12-10 20:03:47 +0000

    Add files via upload

---
+++ decode.js
@@ -0,0 +1,4 @@
+var chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var codes = [32, 17, 41, 18, 52, 98, 21, 68, 52, 38, 54, 85, 99, 70, 94, 35, 22, 33, 85, 43, 66, 20, 62, 48, 37, 34]
+console.log(chars.length)
+console.log(codes.length)
